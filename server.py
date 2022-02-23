e04e9a28bd2149effd090d348b94f09b1d59a7d0
taylormeador <taylor.r.meador@gmail.com>
2022-02-23 15:08:23 -0700

    troubleshoot auth0 request

--- server.py
+++ server.py
@@ -17,6 +17,14 @@ import redis
 app = Flask(__name__)
 app.secret_key = os.getenv('FLASK_APP_SECRET')
 
+# Configure Redis for storing the session data on the server-side
+redis_url = os.getenv('REDISTOGO_URL')
+redis = redis.from_url(redis_url)
+app.config['SESSION_TYPE'] = 'redis'
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_USE_SIGNER'] = True
+app.config['SESSION_REDIS'] = redis
+
 oauth = OAuth(app)
 
 auth0 = oauth.register(
@@ -30,14 +38,6 @@ auth0 = oauth.register(
         'scope': 'openid profile email',
     },
 )
-
-# Configure Redis for storing the session data on the server-side
-redis_url = os.getenv('REDISTOGO_URL')
-redis = redis.from_url(redis_url)
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_REDIS'] = redis
 
 
 def requires_auth(f):
