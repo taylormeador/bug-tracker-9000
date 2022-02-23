be92398799150b6e2cbcb3905f412c31fe846938
taylormeador <taylor.r.meador@gmail.com>
2022-02-23 14:50:27 -0700

    troubleshoot auth0 request

--- server.py
+++ server.py
@@ -56,7 +56,7 @@ def requires_auth(f):
 def callback_handling():
     # Handles response from token endpoint
     auth0.authorize_access_token()
-    resp = auth0.get('userinfo')
+    resp = auth0['userinfo']
     userinfo = resp.json()
 
     # Store the user information in flask session.
